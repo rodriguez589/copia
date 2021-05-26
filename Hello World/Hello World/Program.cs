3031344270db483a20c4343e3a7a8de537964f8e
rodriguez589 <62669121+rodriguez589@users.noreply.github.com>
2021-05-25 22:07:16 -0600

    VIVA POO

---
+++ Hello World/Hello World/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bienvenido a contadores y acumuladore");
+            Console.WriteLine("digite un numero N:");
+            int n = Convert.ToInt32 (Console.ReadLine());
+            
+            int resta = 1;
+        
+           for(int i = 4; i>= n; i= 4)
+
+            Console.WriteLine("i:" + (i - resta));
+
+        }
+    }
+}
